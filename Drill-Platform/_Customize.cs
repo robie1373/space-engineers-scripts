145dcc339a30ebde9bcf1fd558847b5fdbec2d3a
Robie <robie1373@gmail.com>
2023-01-17 19:23:04 +0000

    replaced missing customize

--- Drill-Platform/_Customize.cs
+++ Drill-Platform/_Customize.cs
@@ -2,3 +2,6 @@
 
 public const string VERSION = "Template v0.1";
 public const UpdateFrequency FREQ = UpdateFrequency.Update100;
+
+public const string yPistonGroup = "yPistons";
+public const string zPistonGroup = "zPistons";
