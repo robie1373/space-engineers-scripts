aff2311ccd9c4b0222d42bd260e152ad598b511c
Robie <robie1373@gmail.com>
2023-01-17 13:11:40 +0000

    set up variables

--- Template/Template_v0.1/_Customize.cs
+++ Template/Template_v0.1/_Customize.cs
@@ -2,3 +2,21 @@
 
 public const string VERSION = "Template v0.1";
 public const UpdateFrequency FREQ = UpdateFrequency.Update100;
+
+// Set up the X, Y, and Z axis drill group names here.
+// X and Z are the axis running parralel to the ground.
+// If standing at the point where the contraption attaches to the
+// platform, X+ is to your right, Z+ is ahead of you, and Y+ is up.
+
+public const string xPistonGroup = "Drill pistons x";
+public const string yPistonGroup = "Drill pistons y";
+public const string zPistonGroup = "Drill pistons z";
+
+// Define the velocities for the various actions
+
+public const float xExtensionVelocity = .5;
+public const float xRetractionVelocity = .5;
+public const float yExtensionVelocity = .01;
+public const float yRetractionVelocity = 1;
+public const float zExtensionVelocity = .5;
+public const float zRetractinoVelocity = .5;
